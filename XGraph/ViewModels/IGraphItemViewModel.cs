7eb9a44f07edba2703cb5705f1a6c40666c910a9
petitpandarouge <comptes.petitpandarouge@gmail.com>
2015-11-12 00:48:23 +0100

    [QLTY] Fix previous commit.

---
+++ XGraph/ViewModels/IGraphItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace XGraph.ViewModels
+{
+    /// <summary>
+    /// This object represents a graph item view model.
+    /// </summary>
+    public interface IGraphItemViewModel
+    {
+        /// <summary>
+        /// Gets the style to apply to the container.
+        /// </summary>
+        Style ContainerStyle
+        {
+            get;
+        }
+    }
+}
