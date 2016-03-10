093c40af214d059c6fc9d9266656e628486dcfcf
petitpandarouge <comptes.petitpandarouge@gmail.com>
2016-03-11 00:49:22 +0100

    [BLD] Removing XGraph as reference of XZoomAndPan. Simplier sample.

--- XZoomAndPan.TestApp/MainWindow.xaml.cs
+++ XZoomAndPan.TestApp/MainWindow.xaml.cs
@@ -12,8 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using XGraph.ViewModels;
-using XZoomAndPan.TestApp.Graph;
 
 namespace XZoomAndPan.TestApp
 {
@@ -26,42 +24,17 @@ namespace XZoomAndPan.TestApp
         {
             this.InitializeComponent();
 
-            GraphViewModel lViewModel = this.CreateTypeGraph();
-            this.GraphView.DataContext = lViewModel;
-            this.Overvierview.DataContext = lViewModel;
-        }
+            List<String> lModel = new List<string>();
+            lModel.Add("Ceci est un exemple.... :)");
+            lModel.Add("Pouet");
+            lModel.Add("Pouet");
+            lModel.Add("Pouet");
+            lModel.Add("Pouet");
+            lModel.Add("Pouet");
+            lModel.Add("Pouet");
 
-        /// <summary>
-        /// Creates the type graph.
-        /// </summary>
-        /// <returns></returns>
-        public GraphViewModel CreateTypeGraph()
-        {
-            GraphViewModel lGraph = new GraphViewModel();
-            NodeViewModel lNode0 = new TypeNodeViewModel(typeof(SampleClass));
-            lGraph.AddNode(lNode0);
-
-            NodeViewModel lNode1 = new TypeNodeViewModel(typeof(SampleClass));
-            lGraph.AddNode(lNode1);
-
-            NodeViewModel lNode2 = new NodeViewModel();
-            lNode2.DisplayString = "Empty node";
-            lGraph.AddNode(lNode2);
-
-            int i = 0;
-            foreach (NodeViewModel lNode in lGraph.Nodes)
-            {
-                lNode.X = 300 * i;
-                lNode.Y = 100 * i;
-                i++;
-            }
-
-            ConnectionViewModel lConnectionViewModel = new ConnectionViewModel();
-            lConnectionViewModel.Output = lGraph.Nodes.ElementAt(0).Ports.FirstOrDefault(pPort => pPort.Direction == PortDirection.Output);
-            lConnectionViewModel.Input = lGraph.Nodes.ElementAt(1).Ports.FirstOrDefault(pPort => pPort.Direction == PortDirection.Input);
-            lGraph.AddConnection(lConnectionViewModel);
-
-            return lGraph;
+            this.mMainListBox.ItemsSource = lModel;
+            this.mOvervierview.ItemsSource = lModel;
         }
     }
 }
